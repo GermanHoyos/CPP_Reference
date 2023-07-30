51668e5124e52aee6d89eb945ed2893b41f0d309
German Hoyos <hoyos.a.german@gmail.com>
2023-07-30 12:58:33 -0700

    prefect trig circle

--- 07_Easing_Functions_And_Sliders/07_Easing_Functions_And_Sliders.cpp
+++ 07_Easing_Functions_And_Sliders/07_Easing_Functions_And_Sliders.cpp
@@ -3,55 +3,49 @@ using namespace std;
 #include "TimeManager.h"
 #include "InputManager.h"
 
-using namespace std;
-
 int main()
 {
-	Vector2 screen = { 800,600 };
+	Vector2 screen = { 800, 600 };
 	InitWindow(screen.x, screen.y, "C++ Raylib");
-
-	// Textures mus be loaded after the InitWindow and before the while game loop
-	Texture2D ships = LoadTexture("BgLilgShips.png");
-	int textureW = ships.width / 2; //split the image into 2 equal parts
-	int textureH = ships.height;
-
-	// Source rectangle (part of the texture to use for drawing)
-	Rectangle sourceRec = { 0.0f, 0.0f, (float)textureW, (float)textureH };
-
-	// Destination rectangle (screen rectangle where drawing part of texture)
-	Rectangle destRec = { screen.x / 2.0f, screen.y / 2.0f, textureW * 5.0f, textureH * 5.0f };
-
-	// Origin of the texture (rotation/scale point), it's relative to destination rectangle size
-	Vector2 origin = { (float)textureW * 5.0f / 2, (float)textureH * 5.0f / 2 };
-
-	// Change this to rotate textures
-	int rotation = 0;
+	Vector2 circleOrigin = { screen.x / 2, screen.y / 2 };
+	float radius = 200;
+	float angleVar = 0.0f;
+	float angle = angleVar / 180.0f * PI; // scalar value in radians	
 
 	SetTargetFPS(60);
-	while (WindowShouldClose() == false) {
+	while (!WindowShouldClose()) {
 		BeginDrawing();
-		ClearBackground({ 29,43,83,225 });
-		// Time mechanics
+		ClearBackground({ 29, 43, 83, 225 });
 		TimeUtils::displaySystemTime();
-
-		// Input mechanics
 		InputUtils::printKey();
 
-		// Draw objects
-		DrawTexturePro(ships, sourceRec, destRec, origin, (float)rotation, WHITE);
+		// Change Angle:
+		angleVar++; // clockwise
+		angle = angleVar / 180.0f * PI;
+		if (angleVar >= 360) {angleVar = 0;}
 
-		rotation++;
-
-
-
-
-
-
-
+		// Circle:
+		DrawCircleV(circleOrigin, radius, BLACK);
+		// Cos:
+		DrawLineV(
+			{circleOrigin.x,circleOrigin.y}, //start
+			{circleOrigin.x + radius * cosf(angle), circleOrigin.y}, //end
+			RED //color
+		);
+		// Sin:
+		DrawLineV(
+			{circleOrigin.x + radius * cosf(angle),circleOrigin.y}, //start
+			{circleOrigin.x + radius * cosf(angle),circleOrigin.y + radius * sinf(angle)}, //end
+			GREEN //color
+		);
+		// Angle:
+		DrawLineV(
+			{circleOrigin.x,circleOrigin.y}, //start
+			{circleOrigin.x + radius * cosf(angle),circleOrigin.y + radius * sinf(angle)}, //end
+			WHITE
+		);
 		EndDrawing();
 	}
-
-	UnloadTexture(ships);
 	CloseWindow();
 	return 0;
 }
