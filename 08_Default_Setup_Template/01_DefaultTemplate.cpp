9778f35f29d6e2af031d2b9de622e15231ff4c5a
German Hoyos <hoyos.a.german@gmail.com>
2023-08-02 19:50:18 -0700

    angleToDirection function completed

--- 08_Default_Setup_Template/01_DefaultTemplate.cpp
+++ 08_Default_Setup_Template/01_DefaultTemplate.cpp
@@ -4,11 +4,14 @@
 
 using namespace std;
 
-//Test functions
-Vector2 angToRadiansDirection(float angle) {
-	Vector2 direction = { cosf(angle), sin(angle) };
+Vector2 angleToRadians(float angle, Vector2 origin, float radius){
+	angle = angle * 3.15f / 180; //converted to radians
+	Vector2 direction = {(origin.x + radius * cosf(angle)),(origin.y + radius * sinf(angle))};
 	return direction;
-};
+	//TroubleShoot above with:
+	//string dir_str = to_string(direction.x) + " " + to_string(direction.y);
+	//DrawText(dir_str.c_str(), 0, 0, 20, GREEN);
+}
 
 int main() 
 {
@@ -19,19 +22,14 @@ InitWindow(screen.x, screen.y, "C++ Raylib");
 Texture2D ships = LoadTexture("BgLilgShips.png");
 int textureW = ships.width / 2; //split the image into 2 equal parts
 int textureH = ships.height;
-
 // Source rectangle (part of the texture to use for drawing)
 Rectangle sourceRec = { 0.0f, 0.0f, (float)textureW, (float)textureH };
-
 // Destination rectangle (screen rectangle where drawing part of texture)
 Rectangle destRec = { 60, screen.y / 2.0f, textureW * 5.0f, textureH * 5.0f };
-
 // Origin of the texture (rotation/scale point), it's relative to destination rectangle size
 Vector2 origin = { (float)textureW * 5.0f  / 2, (float)textureH * 5.0f / 2 };
-
 // Change this to rotate textures
 int rotation = 0;
-
 // Test Variables (math tests)
 float angle = 0;
 
@@ -41,7 +39,6 @@ while (WindowShouldClose() == false) {
 	ClearBackground({29,43,83,225});
 	// Time mechanics
 	TimeUtils::displaySystemTime();	
-
 	//Input mechanics
 	InputUtils::printKey();
 
@@ -49,19 +46,15 @@ while (WindowShouldClose() == false) {
 	DrawTexturePro(ships, sourceRec, destRec, origin, (float)rotation, WHITE);
 	rotation++;
 
-	//Test functions using circle
-	Vector2 direction;
-	//direction = angToRadiansDirection(75);
-	//float angle = 0;
+	//Test functions using circle, take an angle and display it on a circle
 	angle++;
-	float angleToRad = angle * 3.15 / 180;
-	direction.x = 250 + 50 * cosf(angleToRad);
-	direction.y = 300 - 50 * sinf(angleToRad);
-	string dir_str = to_string(direction.x) + " " + to_string(direction.y);
-	DrawText(dir_str.c_str(),screen.x / 2, 0, 20, GREEN);
-	DrawCircleV({250, 300},50,BLACK);
+	Vector2 origin = {250,300};
+	DrawCircleV(origin,50,BLACK);
+	Vector2 direction = angleToRadians(angle,origin,50);
 	DrawCircleV({direction.x,direction.y},4,GREEN);
+	
 
+	//
 
 
 
