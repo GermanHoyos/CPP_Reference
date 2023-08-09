38615410a4b36515d47b0b4413cc31c9a7757bcc
German Hoyos <hoyos.a.german@gmail.com>
2023-08-08 20:31:00 -0700

    example scanf program complete

---
+++ ANSI_C/4_scanF.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define LIMIT 100 //DEFINES ONLY OFFECT LINES THAT COME AFTER IT
+#define MYPI 3.14159 //"MYPI" = SYMBOLIC CONSTANT
+
+//%d = conversion character
+
+int main(void)
+{
+    char first, middle, last;
+    int age;
+
+    printf("Input your three initials and your age: ");
+    scanf("%c%c%c%d", &first, &middle, &last, &age);
+    printf("\nGreetings %c.%c.%c.  %s %d.\n",
+        first, middle, last,
+        "Next year your age will be", age + 1);
+    return 0;
+}
+
+// notice carefully
+// "%c%c%c%d   &first & middle &last &age"
